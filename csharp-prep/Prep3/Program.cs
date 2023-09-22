d6fbde187c732f120c205ca69b4e4761c5e9e1f9
carolnacher <carolnacher@gmail.com>
2023-09-21 23:46:00 -0300

    w2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        string decision = ""; 
+
+        
+        Console.WriteLine("Hello!, Do you want to play Guess My Number? (yes/no)");
+        decision = Console.ReadLine();
+
+        while (decision == "yes")
+        {
+
+            int user= -1;
+        
+
+        
+            while (user != number)
+            {
+                Console.WriteLine("What is the number");
+                user = int.Parse(Console.ReadLine());
+                if (number>user)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if(number<user)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else {
+                    
+                    Console.WriteLine("It's correct!!");
+                    Console.WriteLine("Would you like to continue playing Guess My Number? (yes/no)");
+
+                    decision = Console.ReadLine();
+                }
+                
+            }
+ 
+        }
+        
+        Console.WriteLine("oh. It's so sad!! see you the next timet");
     }
+
+
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,96 @@
 using System;
+using System.Net.WebSockets;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        List<int> positives =  new List<int>();
+        int new_number = -1;
+        
+        
+        while (new_number!=0) 
+        {
+
+            Console.WriteLine("Please input a list of numbers if you don't want to just type '0' to stop.");
+            new_number = int.Parse(Console.ReadLine());
+
+            if (new_number== 0)
+            {
+                Console.WriteLine("Good bye!!");
+                return;
+                
+            }
+
+            else 
+            {
+                while (new_number != 0 )
+                {
+                    if (new_number !=0)
+                    {
+                        numbers.Add(new_number);
+
+                        if (new_number>0)
+                        {
+                            positives.Add(new_number);
+                        }
+
+                    }
+                Console.WriteLine("Enter a new numbers, if you don't want to just type '0' to stop.");
+                new_number = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int max = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The max is: {max}");
+
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+
+            int min = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                {
+                    
+                    min = number;
+                }
+            }
+
+            Console.WriteLine($"The min is: {min}");
+
+            foreach (int number in numbers)
+            {
+            Console.WriteLine(number);
+            }
+        }   
+
+
+
     }
+
 }
