dfeb47881a8b3d7760456aa816215a837f239b4d
carolnacher <carolnacher@gmail.com>
2023-09-29 00:49:56 -0300

    w03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+
+       Job job1 = new Job();
+       job1._jobTitle =  "Software Engineer";
+       job1._company = "Microsoft";
+       job1._startYear = 2010;
+       job1._endYear = 2017;
+
+       
+       
+       Job job2 = new Job();
+       job2._jobTitle= "Manager" ;
+       job2._company = "Apple";
+       job2._startYear = 2014;
+       job2._endYear = 2022;
+       
+
+       Resume myResume = new Resume();
+
+       myResume._name = "Alison Rose";
+       myResume._JobsList.Add(job1);
+       myResume._JobsList.Add(job2);
+    
+    
+       Console.WriteLine("Hello Learning02 World!");
+
+       myResume.Display();
+
+       
+
+
+       
+
+        
+       
+       }
 }
